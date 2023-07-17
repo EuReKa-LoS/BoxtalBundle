b5cadb9a57a9dd91d7421de8803717af71573f28
EuReKa-LoS <adn.lan.90@gmail.com>
2023-07-17 19:20:36 +0200

    Fix bundle fonction pas vital

--- src/EureKaLosBoxtalBundle.php
+++ src/EureKaLosBoxtalBundle.php
@@ -11,16 +11,5 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EurekaLosBoxtalBundle extends Bundle
 {
-    public static function register()
-    {
-        spl_autoload_register(array(__CLASS__,'autoload'));
-    }
-
-    public static function autoload($class)
-    {
-        $class = str_replace('Emc\\', '', $class);
-        $class = str_replace('\\', '/', $class);
-        $class = str_replace('Env', '', $class);
-        require $class .'.php';
-    }
+    
 }
