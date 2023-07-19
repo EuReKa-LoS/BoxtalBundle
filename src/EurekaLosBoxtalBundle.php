b53c9d3cf9f312093a1fdb1febc07a7cd006bd39
EuReKa-LoS <adn.lan.90@gmail.com>
2023-07-19 17:41:01 +0200

    Empty missing file fix

---
+++ src/EurekaLosBoxtalBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Atempt of creation bundle for Symfony 6
+
+*/
+
+namespace EureKaLos\BoxtalBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EurekaLosBoxtalBundle extends Bundle
+{
+    
+}
